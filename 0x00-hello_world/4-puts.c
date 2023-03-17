52347cc2fb45edbd8a9c55b71fb909d442e5c64b
SAINTDOMINICK <dominicadesanya109@gmail.com>
2023-03-17 14:36:37 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,11 @@
-#include < stdio.h
+#include<stdio.h>
+/**
+ *main - this is the main function tat contained the code
+ *
+ * Return: it return 0 always
+ **/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
