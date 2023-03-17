a262b5c61d24f06f4b3610d224c8420217176116
SAINTDOMINICK <dominicadesanya109@gmail.com>
2023-03-17 15:03:54 -0700

    c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - this is where te runtime going to start executing the program
+ * Return: always return 0 integer
+ **/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
