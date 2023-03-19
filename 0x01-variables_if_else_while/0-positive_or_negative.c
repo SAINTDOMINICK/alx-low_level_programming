34ff3fdc1ffa302c4a087702324f68528e6a2b88
SAINTDOMINICK <dominicadesanya109@gmail.com>
2023-03-19 08:41:57 -0700

    c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -21,9 +21,10 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
-	else (n < 0)
+	else
 	{
 		printf("%d is negative\n", n);
 	}
+
 	return (0);
 }
