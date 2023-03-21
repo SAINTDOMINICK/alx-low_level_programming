901fafc37e9c41e890ee6fb8aba5baaed46faae8
SAINTDOMINICK <dominicadesanya109@gmail.com>
2023-03-21 14:25:29 -0700

    README.md

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * prints the alphabet in lower case
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	while (i - 97; i <= 122; i++)
+	{
+
+		-putchar(i);
+	}
+	-putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value
+ * of an imteger.
+ *
+ * @i: input number as an integer.
+ *
+ * Return: absolute value
+ */
+int _abs(int)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+	_putchar('\n')
+}
