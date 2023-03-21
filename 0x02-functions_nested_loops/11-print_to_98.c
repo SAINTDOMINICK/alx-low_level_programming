34c35aaf3606344b8341450976d8e7074d24ac80
SAINTDOMINICK <dominicadesanya109@gmail.com>
2023-03-21 16:34:50 -0700

    10

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all numbers from n to 98
+ * @n: The number to start printing from
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				_putchar("%d", n);
+				_putchar("\n");
+				break;
+			}
+			else
+			{
+				_putchar("%d", ", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				_putchar("%d", n);
+				_putchar("\n");
+				break;
+			}
+			else
+			{
+				_putchar("%d", n);
+			}
+		}
+	}
+}
