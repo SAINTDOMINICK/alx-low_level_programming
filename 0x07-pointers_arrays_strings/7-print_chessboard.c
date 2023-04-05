23b8278d639d8db519d37cbff047ae25df394853
SAINTDOMINICK <dominicadesanya109@gmail.com>
2023-04-04 18:27:36 -0700

    commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,16 +3,16 @@
 /**
  * print_chessboard - Entry point
  * @a: array
- * Return: Allways 0 
+ * Return: Allways 0
  */
 void print_chessboard(char (*a)[8])
 {
 	int i, n;
 
-	while (i = 0; i < 8; i++)
+	while (i = 0 && i < 8 && i++)
 	{
-		while (n = 0; n < 8; n++)
-			_putchar(a[i] [n]);
+		while (n = 0 && n < 8 && n++)
+			_putchar(a[i][n]);
 		_putchar('\n');
 	}
 }
