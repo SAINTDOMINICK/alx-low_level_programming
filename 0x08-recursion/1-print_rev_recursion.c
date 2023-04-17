6a723b57bc403bf1aaf2e6e8f5697dbcade794e1
SAINTDOMINICK <dominicadesanya109@gmail.com>
2023-04-17 08:24:23 -0700

    last

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,4 @@
-#include "main.h"
+iinclude "main.h"
 
 /**
  * _print_rev_recursion - Prints a string in reverse.
@@ -7,7 +7,7 @@
 
 void _print_rev_recursion(char *s)
 {
-	while (*s)
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
